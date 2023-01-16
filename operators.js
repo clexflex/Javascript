c2604abd7e8f7bfd4aa0fefc3c65516ed10b253e
yashraj <yashrajg1322@gmail.com>
2023-01-16 21:50:52 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -1,1 +1,54 @@
+
+//@ts-check
 //Operators in JS
+console.log("Arithmatic Operator")
+let a=10;
+let b=20;
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a / b =",a/b)
+console.log("a * b =",a*b)
+console.log("a % b =",a%b)
+console.log("a ** b =",a**b) //a to the power b
+console.log("a :",a)        //before
+console.log("a ++ =",a++)
+console.log("a :",a)        //after
+
+console.log("++a =",++a)
+console.log("a :",a) 
+console.log("a -- =",b--)
+
+
+console.log("Assignment Operators  =")
+
+let no=8
+console.log(no)
+
+
+console.log("Comparison Operator")
+
+let no1=25;
+let no2="25";
+
+console.log("no1 == no2", no1 == no2)
+console.log("no1 === no2", no1 === no2)
+
+console.log("no1 != no2", no1 != no2)
+console.log("no1 !== no2", no1 !== no2)
+
+console.log("is no1 > no2 ?", no1 > no2)
+console.log("is no1 < no2 ?", no1 < no2)
+
+
+console.log("Logical Operator | works on boolean  ' && || !' ")
+
+let x=5
+let y=7
+console.log(x < y && y > x)
+console.log(x < y || y > x)
+console.log(!false)
+
+
+
+ /*Comments*/
+ //comments
